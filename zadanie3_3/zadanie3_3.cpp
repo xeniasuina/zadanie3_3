9170c96883dffedc9247b4f98fed02f1d06edaaa
Ксения Суина <KDSuina@stud.kpfu.ru>
2021-10-22 21:57:39 +0300

    Добавьте файлы проекта.

---
+++ zadanie3_3/zadanie3_3.cpp
@@ -0,0 +1,23 @@
+﻿//Напишите рекурсивную функцию, которая раскладывает число на простые сомножители.
+#include <iostream>
+using namespace std;
+
+void f3(int k, int div) {
+    if (k == 1) return;
+    if (k % div == 0){
+        cout << div << " ";
+        f3(k / div, div);
+    }
+    else
+        if (div == 2)
+            f3(k, div + 1);
+        else
+            f3(k, div + 2);
+}
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите число ";
+    cin >> n;
+    f3(n, 2);
+}
